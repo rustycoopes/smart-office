abb91a85ef5497796b27a6942318cadeb40ce751
Russ Cooper <russell.cooper@btinternet.com>
2022-03-12 13:14:22 -0500

    initial commit

---
+++ heater.py
@@ -0,0 +1,2 @@
+import requests
+response = requests.get('https://maker.ifttt.com/trigger/toggle_office_heater/with/key/lXcKsyYmtY3Q9tQtn8xsQNr2fzbEO4OHNDqfy2Nph7E')
---
+++ lights.py
@@ -0,0 +1,2 @@
+import requests
+response = requests.get('https://maker.ifttt.com/trigger/work_mode_toggle_lights/with/key/lXcKsyYmtY3Q9tQtn8xsQNr2fzbEO4OHNDqfy2Nph7E')
